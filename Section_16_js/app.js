67e803c7c188610887b95871a18659d075778fdd
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-05 15:41:36 -0500

    and

--- Section_16_js/app.js
+++ Section_16_js/app.js
@@ -8,11 +8,19 @@ let random = Math.random();
 if (random < 0.5) {
     console.log("Congratulations!")
 }
-const age = 34;
+const age = 9;
 if (age < 5) {
     console.log("You are a baby")
 } else if (age < 10) {
     console.log("You are a child")
 } else if (age < 65) {
     console.log("You are an adult")
+} else {
+    console.log("You are a senior")
+}
+const password = prompt("Enter password");
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+    console.log("valid password")
+} else {
+    console.log("incorrect password")
 }
