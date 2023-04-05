0870002ac796b40d16803e9476d7d3d28f9b7f3b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-05 14:13:15 -0500

    Else if

--- Section_16_js/app.js
+++ Section_16_js/app.js
@@ -1,2 +1,18 @@
 console.log("Hello js world!");
 let total = 1 + 3;
+console.log("Goodbye!")
+if (1 + 1 === 2) {
+    console.log("Ukraine won!")
+}
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Congratulations!")
+}
+const age = 34;
+if (age < 5) {
+    console.log("You are a baby")
+} else if (age < 10) {
+    console.log("You are a child")
+} else if (age < 65) {
+    console.log("You are an adult")
+}
