1cb0ed2ba6e812e6b168d344e6579ef4ecd2a65e
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-17 11:53:25 -0500

    While Loop added

--- Section_19_js/Index.js
+++ Section_19_js/Index.js
@@ -1,3 +1,6 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum)
