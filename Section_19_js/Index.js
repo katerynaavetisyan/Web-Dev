f16e3829323e3cd467267fce0132e3e395cda977
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-18 14:17:23 -0500

    Final

--- Section_19_js/Index.js
+++ Section_19_js/Index.js
@@ -3,4 +3,17 @@ while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"));
 }
 const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum)
+
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (guess !== targetNum) {
+    attempts++;
+    if (guess > targetNum) {
+        guess = parseInt(prompt("Too high! Enter new guess!"));
+    } else {
+        guess = parseInt(prompt("Too low! Enter a new guess:"));
+    }
+}
+console.log(`You got it! It took you ${attempts} guesses`)
