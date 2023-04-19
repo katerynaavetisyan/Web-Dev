c4b59e865863aadbd165a26777be4953db979a20
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-19 16:37:35 -0500

    Create sum function and test it

---
+++ Section_20_js/app.js
@@ -0,0 +1,15 @@
+function greet(firstName, lastName) {
+    console.log(`firstName is: ${firstName} ${lastName}`)
+}
+
+//create a function that is called "sum" and accepts two arguments
+//both agruments are numbers
+//function needs to return sum of those arguments
+
+function sum(numOne, numTwo) {
+    let total = numOne + numTwo;
+    return total;
+}
+const result = sum(2, 2);
+
+
