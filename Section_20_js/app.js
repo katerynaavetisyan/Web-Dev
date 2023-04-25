5a9c1b60d2c97500993abac39d2c98be60802d6c
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-25 11:53:53 -0500

    Exercises

--- Section_20_js/app.js
+++ Section_20_js/app.js
@@ -29,3 +29,50 @@ function isSnakeEyes(n1, n2) {
         console.log("Not Snake Eyes!");
     }
 }
+function multiply(x, y) {
+    return x * y;
+
+}
+function isShortsWeather(temperature) {
+    return temperature >= 75;
+}
+
+
+let array = [3, 5, 7];
+
+function lastElement(array) {
+    return array.length ? array[array.length - 1] : null;
+}
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+console.log(capitalize('eggplant'));
+console.log(capitalize('pamplemousse'));
+console.log(capitalize('squid'));
+
+function sumArray(arr) {
+    var total = 0;
+    arr.forEach(function (element) {
+        // 'element' in the parenthesis can be any name
+        total += element;
+    });
+    return total;
+}
+
+const weekDAY = new Array(7);
+weekDAY[1] = 'Sunday';
+weekDAY[2] = 'Monday';
+weekDAY[3] = 'Tuesday';
+weekDAY[4] = 'Wednesday';
+weekDAY[5] = 'Thursday';
+weekDAY[6] = 'Friday';
+weekDAY[7] = 'Saturday';
+
+function returnDay(x) {
+} if (x <= 1 || x >= 7) {
+    return null;
+}
+else {
+    return weekDAY[x];
+}
