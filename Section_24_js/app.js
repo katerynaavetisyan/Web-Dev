d3e7a2b8f88438a388a25cb46c1172518323f0a8
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-04-27 13:11:20 -0500

    querySelector Practice

--- Section_24_js/app.js
+++ Section_24_js/app.js
@@ -1,3 +1,2 @@
-console.dir(document)
-const image = document.getElementById('unicorn');
-const heading = document.getElementById('mainheading');
+const doneTodos = document.querySelectorAll('.done')
+const checkbox = document.querySelector('input[type=checkbox]')
