fa091c8d5ffae106ddab198c809c4eaf685ac57b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-05-04 12:36:26 -0500

    Random Color Exercise

--- Section_25_js/app.js
+++ Section_25_js/app.js
@@ -1,8 +1,14 @@
-const bh = document.querySelector('#hello');
-bh.addEventListener('click', function () {
-    console.log("hello")
+const button = document.querySelector('button');
+const h2 = document.querySelector('h2');
+button.addEventListener('click', function () {
+    const newColor = makeRandomColor()
+
+    document.body.style.backgroundColor = newColor;
+    h2.textContent = newColor;
 });
-const bye = document.querySelector('#goodbye');
-bye.addEventListener('click', function () {
-    console.log("goodbye")
-})
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 225);
+    const g = Math.floor(Math.random() * 225);
+    const b = Math.floor(Math.random() * 225);
+    return `  rgb(${r}, ${g}, ${b})`;
+}
